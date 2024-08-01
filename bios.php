07673cde03bb58b97760873a063bdc6808ded012
Diane Dushimimana <dushimediane12@gmail.com>
2024-08-01 21:04:06 +0200

    add bios components

---
+++ bios.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SOBER PRIZE</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center ">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.html"><span>Sober Club</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="active " href="index.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="services.php">Services</a></li>
+          <li><a class="active" href="our team.php">Our Team</a></li>
+          <!-- <li><a href="portfolio.html">Portfolio</a></li> -->
+          <li><a href="team.php">Incoming and outgoing awards</a></li>
+          <li><a href="prize.php">Sober Prize</a></li>
+          <li><a href="bios.php">Bios</a></li>
+
+          <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+
+      </nav>
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Blog Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>SOBER PRIZE</h2>
+
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>Bios</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Blog Section -->
+
+    <!-- ======= Blog Section ======= -->
+    <section class="service-details">
+
+<div class="container">
+  <h5 class="card-title"><a href="#"><h2><i>Photoes and Bios of Some Members</i></h2></a></h5>
+  <div class="row">
+
+  <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+      <div class="card">
+        <div class="card-img">
+          <img src="assets/img/26.jpeg" alt="..." style="width: 500px; height: 500px;">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title"><a href="#">Hon. Amb. Naeem Khan (CSP, FSP)</a></h5>
+          <p class="card-text">Hon. Naeem Khan is the current High Commissioner and Ambassador to Rwanda. With an illustrious career in diplomacy, he has held several prominent positions, including Director General Consular Affairs, Overseas Pakistanis, and Crisis Management at MOFA, and Consul General in Afghanistan.
+He has served as Counsellor and Head of Chancery at the High Commission of Pakistan in Ottawa, Canada, and has held key roles in Havana, Cuba, Buenos Aires, Argentina, and Madrid, Spain. His extensive experience includes acting as Deputy Chief of Protocol and Director for Canada and Latin America at the Ministry of Foreign Affairs in Islamabad, Pakistan.
+Hon. 
+              </p>
+          <div class="read-more"><a href="Desmond.php"><i class="bi bi-arrow-right"></i> Read More</a></div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+      <div class="card">
+        <div class="card-img">
+          <img src="assets/img/20.jpeg" alt="..." style="width: 500px; height: 500px;">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title"><a href="#">Fortune Daniel Molokele</a></h5>
+          <p class="card-text">Fortune Daniel Molokele is a human rights lawyer who is based at Whange in Zimbabwe 🇿🇼
+
+He is the founder and managing partner of the Molokele Law Chambers that is located at Whange
+
+Prior to that, he also practiced with Mavhunga and Associates and also with Ben Baron and Partners
+
+He graduated with an LLB Honours degree from the University of Zimbabwe in 1999.
+
+During 2008, he also obtained an LLM degree specializing on Fundamental Rights Litigation from the
+University of South Africa
+
+Then in 2020 he graduated with a Masters of Business Administration (MBA) degree with the African
+Leadership University in Kigali, Rwanda
+
+Over the years.
+</p>
+          <div class="read-more"><a href="fernture.php"><i class="bi bi-arrow-right"></i> Read More</a></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+      <div class="card">
+        <div class="card-img">
+        <img src="assets/img/19.jpg" alt="..." style="width: 500px; height: 600px;">
+              </div>
+        <div class="card-body">
+          <h5 class="card-title"><a href="#">Desmond Michael Coverley, Ph.D</a></h5>
+          <p class="card-text">Dr. Desmond Michael Coverley was born in Grand Turk, Turks and Caicos Islands, and is a citizen of the
+              United States.
+              Dr. Coverley’s educational pursuits include a high school diploma from H. J. Robinson High School, Grand
+              Turk; a degree in Religious and Christian Education from Blue Water Bible College.
+              </p>
+          <div class="read-more"><a href="Desmond.php"><i class="bi bi-arrow-right"></i> Read More</a></div>
+        </div>
+      </div>
+
+    </div>
+    <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+      <div class="card">
+        <div class="card-img">
+          <img src="assets/img/24.jpeg" alt="..." style="width: 500px; height: 600px;">
+        </div>
+
+        <div class="card-body">
+          <h5 class="card-title"><a href="#">Mrs. Juliana Obiageli Saka</a></h5>
+          <p class="card-text">Mrs. Juliana Obiageli Saka is an accomplished professional with a
+              Masters in Transport Planning from Lagos State University. Her
+              expertise extends to Maritime and Port Security, with certificates
+              from the Galilee International Management Institute in Israel and the
+              Arab Academy for Science, Technology & Maritime Transport in Egypt.
+              She has undergone specialized training in Port Operations in Belgium
+              and Port Regulations,Compliance, and Performance in South Africa.
+
+              </p>
+          <div class="read-more"><a href="juliane.php"><i class="bi bi-arrow-right"></i> Read More</a></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+      <div class="card">
+        <div class="card-img">
+          <img src="assets/img/b.jpg" alt="..." style="width: 500px; height: 600px;">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title"><a href="#">Hon.Tapera Saiz</a></h5>
+          <p class="card-text">Hon.Tapera Saizi, former Member Of Parliament in Zimbabwe (2018-2023). He was a member of the Health and
+              Environment Portfolio committees. He is a champion for Africa TB Caucus. Before joining parliament he
+              worked as Health Services Administrator for 18 years. He is a holder of Bachelor of Administration
+              (UNISA), MBA with Zimbabwe Open University.</p>
+          <div class="read-more"><a href="#"><i class="bi bi-arrow-right"></i> Read More</a></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
+      <div class="card">
+        <div class="card-img">
+          <img src="assets/img/a.jpg" alt="...">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title"><a href="#">Dr Shakespear Hamauswa</a></h5>
+          <p class="card-text">     Dr Shakespear Hamauswa is a Member of Parliament in Zimbabwe. He hold a PhD in Development Studies, an MSc
+              in International Relations and a Bsc Political Science. He envisages an inclusive society with equal
+              opportunities for all.</p>
+          <div class="read-more"><a href="#"><i class="bi bi-arrow-right"></i> Read More</a></div>
+        </div>
+      </div>
+    </div>
+  
+
+  </div>
+
+</div>
+</section>
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Other health activities</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Cultural education</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Other social education without accommodation</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#"> arts and entertainment activities</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Creative</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              Kagarama <br>
+              Kigali city, Kicukiro<br>
+              Rwanda <br><br>
+              <strong>Phone:</strong> +250788872060<br>
+              <strong>Email:</strong> soberclubrw@gmail.com<br>
+            </p>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>About SOBER CLUB</h3>
+            <p>The strength of the Sober Club lies in its diverse membership, each with a unique story of
+              triumph over alcohol. Among the members are two pastors who, at the age of 13, baptized the
+              Founder of Sober Club Dr. Gamariel Mbonimana and helped embraced a born-again lifestyle.</p>
+            <div class="social-links mt-3">
+              <a href="https://twitter.com/SoberClub2024" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="https://www.facebook.com/profile.php?id=61555333566358" class="facebook"><i
+                  class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="https://www.linkedin.com/in/sober-club-1035602a9/" class="linkedin"><i
+                  class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
